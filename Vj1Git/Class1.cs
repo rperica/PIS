10cbc3558a0b174a4984d58debe18027193ac443
Roko Perica <36516651+rperica@users.noreply.github.com>
2023-03-16 15:41:09 +0100

    Added class

---
+++ Vj1Git/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vj1Git
+{
+    class Class1
+    {
+        public void Foo()
+        {
+            Console.WriteLine("Foo");
+        }
+    }
+}
