62b7e7d7e8458f5b8373ada057d65787ef8daa83
Luka Pelivan <92396416+lpeliv@users.noreply.github.com>
2023-03-16 15:38:20 +0100

    Updated

--- Vj1Git/Program.cs
+++ Vj1Git/Program.cs
@@ -6,7 +6,7 @@ namespace Vj1Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi!");
         }
     }
 }
